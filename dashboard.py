135e6dfc049c1b974e1edf6a2d60568d349472c2
Ritesh kumar_datascientist_future <riteshh.107@gmail.com>
2026-04-01 12:23:01 +0530

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -4,20 +4,33 @@ import matplotlib.pyplot as plt
 
 st.title("🎮 Game Analytics Dashboard")
 
+# Safe loading
 try:
     df = pd.read_csv("game_data.csv")
 except:
     st.warning("No data yet! Play the game first.")
     st.stop()
 
+# Agar file empty ho
+if df.empty:
+    st.warning("CSV file is empty!")
+    st.stop()
+
 st.dataframe(df)
 
+# Score Trend
 st.subheader("Score Trend")
-plt.plot(df["score"])
-st.pyplot(plt)
+fig1, ax1 = plt.subplots()
+ax1.plot(df["score"])
+st.pyplot(fig1)
 
+# Level vs Score
 st.subheader("Level vs Score")
-plt.scatter(df["level"], df["score"])
-st.pyplot(plt)
+fig2, ax2 = plt.subplots()
+ax2.scatter(df["level"], df["score"])
+st.pyplot(fig2)
 
+# Stats
+st.subheader("Statistics")
 st.write("Average Score:", df["score"].mean())
+st.write("Max Score:", df["score"].max())
